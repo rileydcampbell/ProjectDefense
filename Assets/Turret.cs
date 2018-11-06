8707b7508312163201a6961b77e3888d74eb1e77
Kyle Zim <kylezimmy@yahoo.com>
2018-11-06 12:47:09 -0800

    Added Turret Functionality

---
+++ Assets/Turret.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform target;
+
+    public float range = 15f;
+    public float turnSpeed = 10f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform rotateAround;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f); // Repeats the update target function every 1/2 second, prevents calling the function 60+ times per second if the method was instead in the update function
+	}
+
+    // Method that iterates through all enemies in range, finds the closest one, and sets it as the target
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(target == null)
+        {
+            return;
+        }
+
+        // Locking onto target and rotating
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(rotateAround.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        rotateAround.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+    //Visual representation of the range in the scene
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
