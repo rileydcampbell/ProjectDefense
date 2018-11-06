b098c92086c21832c3029579410386da578f45f5
Kyle Zim <kylezimmy@yahoo.com>
2018-11-05 21:18:19 -0800

    Project Base
    Base draft

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
+	void Start ()
+    {
+        target = Waypoints.wayPoints[0];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+	}
+
+    void GetNextWayPoint()
+    {
+        if (wayPointIndex >= Waypoints.wayPoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wayPointIndex++;
+        target = Waypoints.wayPoints[wayPointIndex];
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private int waveNumber = 0;
+
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+	}
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
