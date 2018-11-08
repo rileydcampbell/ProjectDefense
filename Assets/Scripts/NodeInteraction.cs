5672169ec98fce3c3057ceface231a02f5a66a9c
Kyle Zim <kylezimmy@yahoo.com>
2018-11-07 21:20:14 -0800

    Added Node Interaction and VRTK Integration

---
+++ Assets/Scripts/NodeInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class NodeInteraction : MonoBehaviour {
+
+    public VRTK_InteractableObject linkedObject;
+
+	protected virtual void OnEnable()
+    {
+        print("Success");
+    }
+
+    protected virtual void OnDisable()
+    {
+        print("Off");
+    }
+}
---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class NodeScript : MonoBehaviour {
+
+    public Color hoverColor;
+    private Color startColor;
+
+    private GameObject turret = null;
+
+    private Renderer rend;
+    
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void ControllerStartUseInteractableObject()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null){
+            //Cant build here
+            return;
+        }
+    }
+
+}
