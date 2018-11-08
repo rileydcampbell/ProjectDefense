66dc1b3f22f9613d1eb44d1b39136c4ae03e194e
Kyle Zim <kylezimmy@yahoo.com>
2018-11-07 22:26:15 -0800

    Full VR Teleportation and Tower Creation Implemented

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject getTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
--- Assets/Scripts/NodeInteraction.cs
+++ Assets/Scripts/NodeInteraction.cs
@@ -7,13 +7,49 @@ public class NodeInteraction : MonoBehaviour {
 
     public VRTK_InteractableObject linkedObject;
 
+    private GameObject turret;
+
 	protected virtual void OnEnable()
     {
-        print("Success");
+        linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+        if(linkedObject != null)
+        {
+            linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+            linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+        }
+
+        print("On");
     }
 
     protected virtual void OnDisable()
     {
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+            linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
+        }
+        else
+
         print("Off");
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        if (turret != null)
+        {
+            print("Can't build here");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + new Vector3(0f,0.50f,0f), transform.rotation);
+        print("turret built");
+    }
+
+    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+    {
+        OnDisable();
+        print("test2");
     }
 }
